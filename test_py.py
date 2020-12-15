6d1bf32ce5ffbb047d050a48d423703367c9dc16
cakkinep <cakkinep@redhat.com>
2020-12-15 19:48:54 +0000

    changes to glue job

--- test_py.py
+++ test_py.py
@@ -5,4 +5,4 @@ glueContext = GlueContext(SparkContext.getOrCreate())
 inputDF = glueContext.create_dynamic_frame_from_options(connection_type = "s3", connection_options = {"paths": ["s3://awsglue-datasets/examples/us-legislators/all/memberships.json"]}, format = "json")
 inputDF.toDF().show()
 
-print('Hello again')
+print('Hello again !!')
