e67bcec0377dbc9dc3d4a09442f1c4df3070a3df
cakkinep <cakkinep@redhat.com>
2020-12-17 04:42:06 +0000

    history logging enabled

--- test_py.py
+++ test_py.py
@@ -1,8 +1,12 @@
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 from awsglue.context import GlueContext
 
-glueContext = GlueContext(SparkContext.getOrCreate()) 
+conf = SparkConf()
+conf.setAppName('testApp')
+conf.set("spark.eventLog.enabled", "true")
+conf.set("spark.history.fs.logDirectory","file:/tmp/spark-events")
+sc = SparkContext(conf=conf)
+sc=SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
 inputDF = glueContext.create_dynamic_frame_from_options(connection_type = "s3", connection_options = {"paths": ["s3://awsglue-datasets/examples/us-legislators/all/memberships.json"]}, format = "json")
 inputDF.toDF().show()
-
-print('Hello again !!')
