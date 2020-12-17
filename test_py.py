61f89c879957f51d3fdf18bcac81696447c3ec43
cakkinep <cakkinep@redhat.com>
2020-12-17 14:27:46 +0000

    added comments to the job

--- test_py.py
+++ test_py.py
@@ -8,5 +8,6 @@ conf.set("spark.history.fs.logDirectory","file:/tmp/spark-events")
 sc = SparkContext(conf=conf)
 sc=SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
+# creating input DF
 inputDF = glueContext.create_dynamic_frame_from_options(connection_type = "s3", connection_options = {"paths": ["s3://awsglue-datasets/examples/us-legislators/all/memberships.json"]}, format = "json")
 inputDF.toDF().show()
